59ff2e9b1b9cd48f18dad59744b3d7fc5da797d9
goroundtrip <goroundtrip1@gmail.com>
2025-04-19 04:01:17 +0530

    Updated file to fix bug in form submission logic

--- api/partnership.js
+++ api/partnership.js
@@ -12,7 +12,7 @@ async function getAuthToken() {
     return auth;
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     if (req.method !== 'POST') {
         return res.status(405).json({ message: 'Method not allowed' });
     }
